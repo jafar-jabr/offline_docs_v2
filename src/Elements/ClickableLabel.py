7939195a1469c951ee526644384c2a27efea5497
Jafar Jabr <jafar.jabr@punct.ro>
2019-04-23 01:21:53 +0300

    update

--- src/Elements/ClickableLabel.py
+++ src/Elements/ClickableLabel.py
@@ -13,12 +13,43 @@ class ClickableLabel(QLabel):
         self.setCursor(QCursor(Qt.PointingHandCursor))
         style = """
                        QLabel{
-                           color: %s;
+                           color: #fff;
                            font-size: %s;
                            margin: 0 10px;
-                           text-decoration: underline;
+                           background-color: #445566;
                        }
-                       """ % (SessionWrapper.font_color, SessionWrapper.number_to_size[SessionWrapper.regular_size])
+                       """ % SessionWrapper.number_to_size[SessionWrapper.regular_size]
+        self.setStyleSheet(style)
+        self.setMaximumHeight(45)
+        # self.fixWidth()
+
+    def mousePressEvent(self, ev):
+        if ev.button() == Qt.LeftButton:
+            self.clicked.emit()
+        elif ev.button() == Qt.RightButton:
+            self.rightClicked.emit()
+
+    def fixWidth(self):
+        length = len(self.text())
+        if length > 2:
+            self.setFixedWidth(int(length*9.5))
+
+
+class ActiveLabel(QLabel):
+    clicked = pyqtSignal()
+    rightClicked = pyqtSignal()
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.setCursor(QCursor(Qt.PointingHandCursor))
+        style = """
+                       QLabel{
+                           color: #fff;
+                           font-size: %s;
+                           padding: 10px;
+                           background-color: #445559;
+                       }
+                       """ % (SessionWrapper.number_to_size[SessionWrapper.regular_size])
         self.setStyleSheet(style)
         self.setMaximumHeight(45)
         # self.fixWidth()
