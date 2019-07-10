2364a28dadf1ed7d0e0bce074f0f786da7d17b6f
Jafar jabr <jafar.jabr@punct.ro>
2019-07-10 16:26:34 +0300

    update

---
+++ src/Elements/draggableTextArea.py
@@ -0,0 +1,40 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QTextEdit
+
+
+class DraggableTextArea(QTextEdit):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.setMaximumWidth(200)
+        self.setMaximumHeight(100)
+        # self.setCursor(QCursor(Qt.PointingHandCursor))
+
+    def mousePressEvent(self, event):
+        self.__mousePressPos = None
+        self.__mouseMovePos = None
+        if event.button() == Qt.LeftButton:
+            self.__mousePressPos = event.globalPos()
+            self.__mouseMovePos = event.globalPos()
+
+        super(DraggableTextArea, self).mousePressEvent(event)
+
+    def mouseMoveEvent(self, event):
+        if event.buttons() == Qt.LeftButton:
+            # adjust offset from clicked point to origin of widget
+            currPos = self.mapToGlobal(self.pos())
+            globalPos = event.globalPos()
+            diff = globalPos - self.__mouseMovePos
+            newPos = self.mapFromGlobal(currPos + diff)
+            self.move(newPos)
+            self.__mouseMovePos = globalPos
+
+        super(DraggableTextArea, self).mouseMoveEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if self.__mousePressPos is not None:
+            moved = event.globalPos() - self.__mousePressPos
+            if moved.manhattanLength() > 3:
+                event.ignore()
+                return
+
+        super(DraggableTextArea, self).mouseReleaseEvent(event)
--- src/forms/stickyNotesForm.py
+++ src/forms/stickyNotesForm.py
@@ -1,11 +1,9 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QListWidget, QListWidgetItem, QFrame
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout
 from src.Elements.ClickableIcon import ClickableIcon
-from src.Elements.ClickableLabel import ClickableLabel, ActiveLabel
 from src.Elements.CustomLabel import RegularLabel
 from src.Elements.DateFilterWidget import DateFilterWidget
-from src.Elements.FilterTextBox import FilterTextBox
-from src.Elements.LabeledTextArea import LabeledTextArea
-from src.Elements.LabeledTextBox import LabeledTextBox
+from src.Elements.draggableTextArea import DraggableTextArea
 from src.models.SessionWrapper import SessionWrapper
 
 
@@ -16,17 +14,30 @@ class StickyNotesForm(QWidget):
         self.parent = parent
         self.clinic_id = SessionWrapper.clinic_id
         self.pages_count = 6
-        self.landing_layout = QVBoxLayout()
-        self.landing_layout.setContentsMargins(0, 0, 0, 0) #(left, top, right, bottom)
-        self.landing_layout.setSpacing(0)
+        self.sticky_layout = QVBoxLayout()
+        self.sticky_layout.setContentsMargins(0, 0, 0, 0) #(left, top, right, bottom)
+        self.sticky_layout.setSpacing(0)
         self.pc_width = SessionWrapper.get_dimension('main_window_width')
         self.pc_height = SessionWrapper.get_dimension('main_window_height')
         self.initUI()
 
     def initUI(self):
-        lbl = RegularLabel("This will be sticky notes page")
+        lbl = RegularLabel("Inspired by windows 10 sticky notes")
+        test_input = DraggableTextArea()
+        self.sticky_layout.addWidget(lbl)
         date_select = DateFilterWidget(0, 10)
-        self.landing_layout.addWidget(lbl)
-        self.landing_layout.addWidget(date_select)
-        self.setLayout(self.landing_layout)
+        action_line = QHBoxLayout()
+        add_note_btn = ClickableIcon(50, 40, "./resources/assets/images/add_icon.png", "Add")
+        add_note_btn.clicked.connect(self.add_new_note)
+        action_line.addWidget(date_select)
+        action_line.addWidget(add_note_btn)
+        self.sticky_layout.addLayout(action_line)
+        self.sticky_layout.addWidget(test_input)
+
+        self.sticky_layout.setAlignment(Qt.AlignTop)
+        self.setLayout(self.sticky_layout)
         # self.setS
+
+    def add_new_note(self):
+        test_input = DraggableTextArea()
+        self.sticky_layout.addWidget(test_input)
